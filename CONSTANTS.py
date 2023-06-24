7b7f62e5ed46accaa175770eb50543df713eab8f
MikoMikarro <lopezcuestam@gmail.com>
2023-06-24 20:25:13 +0200

    added the Slice class

---
+++ CONSTANTS.py
@@ -0,0 +1,11 @@
+# Define constants
+length = 306.3 # cm
+outter_diameter = 230.4 # cm
+throat_diameter = 26.2 # cm
+inner_diameter = 40.0 # cm
+
+grosor_pared_interior = 5.0 # cm
+grosor_fluido_exterior = 5.0 # cm
+grosor_pared_exterior = 5.0 # cm
+
+num_elems = 10 # #
---
+++ geometric_functions.py
@@ -0,0 +1,111 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+from CONSTANTS import inner_diameter, length, throat_diameter, outter_diameter, \
+    grosor_pared_interior, grosor_fluido_exterior, grosor_pared_exterior, num_elems
+
+class Slice:
+    def __init__(self, element_num) -> None:
+        self.element_num = element_num
+        self.num_elems = num_elems
+
+        x1 = np.array([0, 10, 20 , 40, 50, length-20 , length])
+        y1 = np.array([inner_diameter/2, inner_diameter/2, inner_diameter/2,  throat_diameter/2, \
+                throat_diameter/2, outter_diameter/2,  outter_diameter/2])
+
+        self.spline = CubicSpline(x1/100, y1/100)
+
+        self.p_interior_fluid       = 0
+        self.T_interior_fluid       = 0
+        self.rho_interior_fluid     = 0
+        self.v_interior_fluid       = 0
+
+        self.T_interior_wall        = 0
+
+        self.p_exterior_fluid       = 0
+        self.T_exterior_fluid       = 0
+        self.rho_exterior_fluid     = 0
+        self.v_exterior_fluid       = 0
+
+        self.t_exterior_wall        = 0
+
+    def lateral_area(self, extra_radius = 0):
+        x_vals = [length/self.num_elems * self.element_num, length/self.num_elems * (self.element_num + 1)]
+        r1, r2 = self.spline(x_vals)
+        r1 += extra_radius
+        r2 += extra_radius
+        dx = length/self.num_elems
+        return np.pi * (r1 + r2) * np.sqrt((r2 - r1)**2 + dx**2)
+
+    @property
+    def lateral_area_interior_fluid(self):
+        self.lateral_area()
+
+    @property
+    def lateral_area_interior_wall(self):
+        self.lateral_area(grosor_pared_interior / 100)
+    
+    @property
+    def lateral_area_exterior_fluid(self):
+        self.lateral_area((grosor_pared_interior + grosor_fluido_exterior)/100)
+
+    @property
+    def lateral_area_exterior_wall(self):
+        self.lateral_area((grosor_pared_interior + grosor_fluido_exterior + grosor_pared_exterior \
+                           )/100)
+    
+    @property
+    def angle(self):
+        x_vals = [length/self.num_elems * self.element_num, length/self.num_elems * (self.element_num + 1)]
+        r1, r2 = self.spline(x_vals)
+        dx = length/self.num_elems
+
+        return np.arctan2(r2 - r1, dx)
+    
+    def cross_section(self, extra_radius = 0):
+        x_vals = [length/self.num_elems * self.element_num]
+        r = self.spline(x_vals) + extra_radius
+        return np.pi * r**2
+    
+    @property
+    def cross_section_interior_fluid(self):
+        return self.cross_section()
+    
+    @property
+    def cross_section_interior_wall(self):
+        return self.cross_section(grosor_pared_interior / 100)
+    
+    @property
+    def cross_section_exterior_fluid(self):
+        return self.cross_section((grosor_pared_interior + grosor_fluido_exterior)/100)
+    
+    @property
+    def cross_section_exterior_wall(self):
+        return self.cross_section((grosor_pared_interior + grosor_fluido_exterior + grosor_pared_exterior \
+                           )/100)
+
+    @property
+    def alpha_interior_fluid(self):
+        return 0
+    
+    @property
+    def alpha_exterior_fluid(self):
+        return 0
+    
+    @property
+    def lambda_interior_fluid(self):
+        return 0
+    
+    @property
+    def lambda_interior_wall(self):
+        return 0
+    
+    @property
+    def lambda_exterior_fluid(self):
+        return 0
+    
+    @property
+    def lambda_exterior_wall(self):
+        return 0
+    
+    
