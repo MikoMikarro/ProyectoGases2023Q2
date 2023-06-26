b81b45277e4cc048ba1c95d3232762e53cb23362
MikoMikarro <lopezcuestam@gmail.com>
2023-06-26 17:33:42 +0200

    arreglado radius

--- geometric_functions.py
+++ geometric_functions.py
@@ -114,7 +114,7 @@ class Slice:
         return lambda_copper(self.T_exterior_wall)
     
     def radius(self):
-        x_vals = [length/self.num_elems * self.element_num]
+        x_vals = [length/self.num_elems * self.element_num] / 100
         r = self.spline(x_vals)
         return r[0]
     
