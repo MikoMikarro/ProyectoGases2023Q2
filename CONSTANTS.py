d6f62d542e6c945576c82cd302419eaab7ece7ef
PauCliment <pcliments@gmail.com>
2023-06-27 13:41:08 +0200

    yorando

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,8 +1,8 @@
 # Define constants
-length = 100.0 # cm
-outter_diameter = 25.4 # cm
-throat_diameter = 20.2 # cm
-inner_diameter = 30.0 # cm
+length = 150.0 # cm
+outter_diameter = 60 # cm
+throat_diameter = 20 # cm
+inner_diameter = 60 # cm
 
 grosor_pared_interior = 5.0 # cm
 grosor_fluido_exterior = 5.0 # cm
@@ -11,7 +11,7 @@ grosor_pared_exterior = 5.0 # cm
 epsilon_r = 0.01 # rugosidad relativa adimensional
 
 # Numeric
-num_elems = 30 #
+num_elems = 500 #
 convergence_T = 0.0001
 convergence_p = 0.0001
 convergence_v = 0.0001
